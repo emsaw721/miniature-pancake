464c4cbe84b4af038172471a06cf5b86d9b56f7a
Emily Hill <emhillmed2021@gmail.com>
2022-12-06 16:42:25 -0600

    finished beginning logic for user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,17 +4,53 @@ const userController = {
     //get all users
     getAllUser(req,res) {
         User.find({})
-        .populate({
-            path: 'thoughts',
-            select: '-__v'
-        })
-        .select('-__v')
-        .sort({ _id: -1 })
         .then(allUsers => res.json(allUsers))
         .catch(err => {
             console.log(err);
             res.status(400).json(err); 
         });
     },
-    
+
+    getUserById({params}, res) {
+        User.findOne({_id: params.id})
+        .populate('reactions')
+        .populate('friends')
+        .then(oneUser => res.json(oneUser))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err); 
+        })
+    },
+
+    createUser({ body}, res) {
+        User.create(body)
+        .then(createdUser => res.json(createdUser))
+        .catch(err => res.status(400).json(err)); 
+    },
+
+    updateUser({params, body}, res) {
+        User.fineOneAndUpdate({_id: params.id}, body, {new:true})
+        .then(updatedUser => {
+            if(!updatedUser) {
+                res.status(404).json({message: 'No user with that id found.'});
+                return; 
+            } 
+            res.json(updatedUser)
+        })
+        .catch(err => res.status(400).json(err)); 
+    },
+
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.id})
+        .then(deletedUser => {
+            if(!deletedUser) {
+                res.status(404).json({message: 'No user with that id found.'});
+                return; 
+            }
+            res.json(deletedUser);
+        })
+        .catch(err => res.status(400).json(err)); 
+    }
 }
+
+module.exports = userController; 
