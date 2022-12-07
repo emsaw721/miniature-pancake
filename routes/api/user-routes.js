528f49ada8a98c58bf5b121da4f161ad5f57023e
Emily Hill <emhillmed2021@gmail.com>
2022-12-06 18:17:16 -0600

    started user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const { get } = require('http');
 const{ getAllUser, getUserById, createUser, updateUser, deleteUser} = require('../../controllers/user-controllers');
 
+router
+.route('/')
+.get(getAllUser)
+.post(createUser);
+
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+module.exports = router; 
